4f89f703b9f319e6170a40cd031e979ea3b5fcad
wadiley <wadmiguel547@yahoo.com>
2026-09-14 15:18:21 +0000

    Versão 14/09.01

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-// Configuração do período de submissão permitido
+// Configuração do período de submissão permitido (Fuso horário de STP / UTC+0)
 const PERIODO_SUBMISSAO = {
-  inicio: new Date('2026-09-01T00:00:00'), // Altere para a data de início desejada
-  fim: new Date('2026-09-13T22:51:59')     // Altere para a data limite desejada
+  inicio: new Date('2026-09-01T00:00:00Z'), // 'Z' indica tempo UTC / STP
+  fim: new Date('2026-09-13T22:51:59Z')
 };
